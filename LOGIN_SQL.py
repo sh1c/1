fb5cba1545239d4252f789362a838c72e11ad05b
Name <email>
2024-12-28 23:57:27 +0800

    addHtml

--- LOGIN_SQL.py
+++ LOGIN_SQL.py
@@ -1,10 +1,10 @@
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,session,render_template
 from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash,check_password_hash
 
 app = Flask(__name__)
-
+app.secret_key = "123456"
 # 配置MySQL数据库连接
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -12,24 +12,6 @@ app.config['MYSQL_PASSWORD'] = '123456'
 app.config['MYSQL_DB'] = 'mydatabase'
 
 mysql = MySQL(app)
-
-# 登录API
-@app.route('/login', methods=['POST'])
-def login():
-    data = request.get_json()
-    username = data['username']
-    password = data['password']
-
-    # 验证用户
-    cursor = mysql.connection.cursor()
-    cursor.execute("SELECT * FROM users WHERE username = %s ", (username,))
-    user = cursor.fetchone()
-    cursor.close()
-
-    if user and check_password_hash(user[2],password) :
-        return jsonify({'message': 'Login successful'}), 200
-    else:
-        return jsonify({'message': 'Invalid username or password'}), 401
 
 # 注册API
 @app.route('/register', methods=['POST'])
@@ -58,5 +40,33 @@ def register():
 
     return jsonify({'message': 'User registered successfully'}), 201
 
+# 登录API
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+
+    # 验证用户
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM users WHERE username = %s ", (username,))
+    user = cursor.fetchone()
+    cursor.close()
+
+    if user and check_password_hash(user[2],password) :
+        session["username"]=username
+        return jsonify({'message': 'Login successful'}), 200
+    else:
+        session.clear()
+        return jsonify({'message': 'Invalid username or password'}), 401
+
+
+#商城页面API
+@app.route("/storeMenu",methods=['GET'])
+def menu():
+    if "username" in session and session["username"] is not None:
+        return render_template("store.html")
+    else:
+        return render_template("404.html")
 if __name__ == '__main__':
     app.run(debug=True)
